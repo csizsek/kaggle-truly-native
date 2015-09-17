36abfbd2e97d768d0fcabe12cfd235e0fcf9d106
Peter Csizsek <peter.csizsek@gmail.com>
2015-09-17 15:19:14 +0200

    fixed feature extractor script

--- generate_features.sh
+++ generate_features.sh
@@ -3,10 +3,10 @@
 input_dir=$1
 output_dir=$2
 
-for i in 0 1 2 3 4 5
+for i in 0 1 2 3 4
 do
-    unzip "$target_dir/$i.zip" -d "$target_dir"
-    python generate_features.py "$target_dir/$i/" "$output_dir/$i.csv"
-    rm -rf "$target_dir/$i/"
+    unzip "$input_dir/$i.zip" -d "$input_dir"
+    python generate_features.py "$input_dir/$i/" "$output_dir/$i.csv"
+    rm -rf "$input_dir/$i/"
 done
 
