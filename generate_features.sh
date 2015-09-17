30cecb08632c7269ebfc2bcfc355f73fb6931975
Peter Csizsek <peter.csizsek@gmail.com>
2015-09-17 14:15:52 +0200

    renamed directories

--- generate_features.sh
+++ generate_features.sh
@@ -1,11 +1,12 @@
 #! /bin/bash -ex
 
-target_dir=$1
+input_dir=$1
+output_dir=$2
 
-for i in 0 1 2 3 4
+for i in 0 1 2 3 4 5
 do
     unzip "$target_dir/$i.zip" -d "$target_dir"
-    python generate_features.py "$target_dir/$i/" "$target_dir/$i.csv"
+    python generate_features.py "$target_dir/$i/" "$output_dir/$i.csv"
     rm -rf "$target_dir/$i/"
 done
 
