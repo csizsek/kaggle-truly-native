03e9e93c6cf0f339b03a437d1a117a730dfd4a50
Peter Csizsek <peter.csizsek@gmail.com>
2015-09-18 12:11:20 +0200

    some very basic feature generation

--- generate_features.py
+++ generate_features.py
@@ -1,56 +1,102 @@
 import datetime
 import os
+import re
 import sys
-
 from bs4 import BeautifulSoup as bs
 
+def clean_text(s):
+    try:
+        s = re.sub(r'[\n\t,.:;()\-\/_]+', ' ', s)
+        s = re.sub(r'\s{2,}', ' ', s)
+        s = s.strip()
+        s = s.lower()
+        return s
+    except Exception as e:
+        #print 'failed to clean up text: {0}'.format(e)
+        return ''
+
 def parse_text(soup):
-    texts = ['']
-    for text in soup.find_all('p'):
-        try:
-            texts.append(text.text.encode('ascii','ignore').strip())
-        except Exception:
-            continue
+    texts = []
+    try:
+        for text in soup.find_all('p'):
+            try:
+                texts.append(clean_text(text.text.encode('ascii','ignore')))
+            except Exception as e:
+                #print 'failed to parse paragraph: {0}'.format(e)
+                pass
+    except Exception as e:
+        #print 'failed to parse text: {0}'.format(e)
+        pass
     return filter(None,texts)
 
 def parse_title(soup):
-    title = ['']
+    title = ''
     try:
-        title.append(soup.title.string.encode('ascii','ignore').strip())
-    except Exception:
-        return title
-    return filter(None,title)
+        title = clean_text(soup.title.string.encode('ascii','ignore'))
+    except Exception as e:
+        #print 'failed to parse title: {0}'.format(e)
+        pass
+    return title
 
 def parse_links(soup):
-    links = ['']
+    links = []
     for link in soup.find_all('a'):
         try:
             links.append(str(link.get('href').encode('ascii','ignore')))
-        except Exception:
-            continue
+        except Exception as e:
+            #print 'failed to parse link: {0}'.format(e)
+            pass
     return filter(None,links)
 
 def parse_images(soup):
-    images = ['']
+    images = []
     for image in soup.findAll("img"):
         try:
             images.append("%(src)s"%image)
-        except Exception:
-            continue
+        except Exception as e:
+            #print 'failed to parse image: {0}'.format(e)
+            pass
     return filter(None,images)
 
 def extract_features(input_dir, file_name):
     with open(input_dir + os.sep + file_name, 'r') as file:
         soup = bs(file, 'html.parser')
-    texts = parse_text(soup)
+
     title = parse_title(soup)
+    n_title_chars = len(title)
+    n_title_words = len(title.split(' '))
+
     links = parse_links(soup)
+    n_links = len(links)
+
     images = parse_images(soup)
+    n_images = len(links)
+
+    texts = ''.join(parse_text(soup))
+    n_paragraphs = len(texts)
+    text = ' '.join(texts)
+    n_chars = len(text)
+    n_words = len(text.split(' '))
+
+    links_per_words = 0
+    if n_words > 10:
+        links_per_words = 1.0 * n_links / n_words
+
+    images_per_words = 0
+    if n_words > 10:
+        images_per_words = 1.0 * n_images / n_words
+
     return [
         file_name,
-        len(images),
-        len(links),
-        len(texts)
+        n_title_chars,
+        n_title_words,
+        n_links,
+        n_images,
+        n_paragraphs,
+        n_chars,
+        n_words,
+        links_per_words,
+        images_per_words
     ]
 
 if __name__ == '__main__':
